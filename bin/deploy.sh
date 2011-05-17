2784c9836445e37d40f78e299425b79a311146a2
Timo Derstappen <teemow@gmail.com>
2011-05-17 13:44:50 +0200

    added npm install to deploy script
    also added sudo commands for deployment.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,7 @@
 
 CURRENT=$(cd `dirname $0`; cd ..; pwd)
 
-rm /etc/munin/plugins/{redis,network,couchdb}*
-ln -s $CURRENT/plugins/{redis,network,couchdb}_* /etc/munin/plugins/
-service munin-node reload
+sudo rm /etc/munin/plugins/{redis,network,couchdb}*
+sudo ln -s $CURRENT/plugins/{redis,network,couchdb}_* /etc/munin/plugins/
+cd $CURRENT; npm install
+sudo service munin-node reload
