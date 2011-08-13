bbc1bc6b9bf968877c0f6bc9ee3f6510bdff24d0
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2011-08-13 20:48:21 +0200

    updated deploy.sh to install ac_* plugins

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,6 @@
 
 CURRENT=$(cd `dirname $0`; cd ..; pwd)
 
-rm /etc/munin/plugins/{redis,network,couchdb}*
-ln -s $CURRENT/plugins/{redis,network,couchdb}_* /etc/munin/plugins/
+rm /etc/munin/plugins/{ac,redis,network,couchdb}*
+ln -s $CURRENT/plugins/{ac,redis,network,couchdb}_* /etc/munin/plugins/
 service munin-node reload
